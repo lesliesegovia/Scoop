4a4edf122cd91ff97f4b389066c3945bc6d36ceb
Leslie Segovia <segovialeslie1030@gmail.com>
2026-06-13 21:39:46 -0700

    setting up project files

---
+++ Scoop/Scoop/ScoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoopApp.swift
+//  Scoop
+//
+//  Created by Marisol Bueno on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct ScoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
